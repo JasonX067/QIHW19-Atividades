06dd28462bff943a4a47aba1e4215fda24f4b5b9
Jason <166767325+JasonX067@users.noreply.github.com>
2024-05-17 16:28:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+n1 = float(input("digite a 1º nota \n "))
+
+n2 = float(input("digite a 2º nota \n" ))
+
+n3 = float(input("digite a 3º nota \n" ))
+
+print("receba")
+
+media = (n1 + n2 + n3) /3
+
+print(media)
+
+if media > 5 :
+  
+  print("Você passou")
+
+else :
+
+  print("Você reprovou, lixo humano")
